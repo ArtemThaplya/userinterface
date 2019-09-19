bf183bf982596550eaabe0ee0fcee311b58fbb24
ArtemThaplya <artem.thaplya@gmail.com>
2019-09-19 08:51:09 +0300

    Create project, pojo

---
+++ src/main/java/com/tsaplya/web/NumberStorage.java
@@ -0,0 +1,13 @@
+package com.tsaplya.web;
+
+public class NumberStorage {
+    private int magicNumber;
+
+    public int getMagicNumber() {
+        return magicNumber;
+    }
+
+    public void setMagicNumber(int magicNumber) {
+        this.magicNumber = magicNumber;
+    }
+}
