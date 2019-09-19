8c6c3cff34c831464fc10da6c91df09cca7f428e
ArtemThaplya <artem.thaplya@gmail.com>
2019-09-19 08:57:53 +0300

    add H2

--- src/main/java/com/tsaplya/web/NumberStorage.java
+++ src/main/java/com/tsaplya/web/NumberStorage.java
@@ -1,7 +1,34 @@
 package com.tsaplya.web;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.*;
+
+@Entity
+@Configuration
+@Table(name = "NumberStorage")
 public class NumberStorage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "magicNumber")
     private int magicNumber;
+
+    @Bean
+    NumberStorage create() {
+        return new NumberStorage();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public int getMagicNumber() {
         return magicNumber;
